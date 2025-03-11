ebb76fe86ef60b5887eb64e034652bde1a631198
lancenonce <lancendavis47@gmail.com>
2025-03-11 13:53:01 +0000

    bookmark: initData still not loading

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,52 +4,49 @@ import Receive from "../pages/Receive/Receive";
 import Transfer from "../pages/Transfer/Transfer";
 import { PayWithQRCode } from "../pages/Transfer/PayWithQRCode";
 import { TransactionHistory } from "../pages/History/TransactionHistory";
+import { initData } from '@telegram-apps/sdk';
 
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useGlobal } from "../contexts/global";
 
-// Define the Telegram WebApp global object
-declare global {
-  interface Window {
-    Telegram: {
-      WebApp: {
-        initData: string;
-        initDataUnsafe: {
-          user?: {
-            id: number;
-            first_name: string;
-            last_name?: string;
-            username?: string;
-            photo_url?: string;
-          };
-        };
-        ready: () => void;
-        expand: () => void;
-        close: () => void;
-      };
-    };
-  }
-}
-
-
 const AppRoutes = () => {
   const { setUserData } = useGlobal();
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      const telegramUser = window.Telegram.WebApp.initData;
+    initData.restore();
+    console.log("AppRoutes useEffect called");
+    const authenticateUser = async () => {
+      console.log("authenticateUser function called");
+      console.log("initData restored:", initData.raw());
+      console.log("user:", initData.user);
 
-      // alert(telegramUser)
-      if (telegramUser) {
-        setUserData(telegramUser);
+      try {
+        const response = await axios.post(
+          "https://511a-89-116-154-84.ngrok-free.app/api/auth",
+          {
+            initData: initData.raw(),
+          }
+        );
+
+        if (response.data.authenticated) {
+          // Set user data in global state
+          setUserData(response.data.user);
+          setLoading(false);
+        } else {
+          throw new Error("Authentication failed");
+        }
+      } catch (err: any) {
+        console.error("Error authenticating user:", err.message || err);
+        setError(err.message || "An unknown error occurred");
+        setLoading(false);
       }
-    }else{
-      setError("Error")
-    }
-  }, []);
+    };
+
+    authenticateUser();
+  }, [setUserData]);
 
   if (loading) {
     return <div className="loading">Loading user data...</div>;
